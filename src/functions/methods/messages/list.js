2d4b0eebf9d0137d127341a4a5ed1ef4af220aed
Kerstin Huppenbauer <k.huppenbauer@web.de>
2020-06-18 07:50:38 +0200

    TASK: Add paging to list methods

--- src/functions/methods/messages/list.js
+++ src/functions/methods/messages/list.js
@@ -4,8 +4,22 @@ const Message = require('../../models/message');
 
 module.exports = async (event) => {
   let result;
+  let num;
+  await Message.estimatedDocumentCount(function (err, count) {
+    num = count;
+  });
+  const page = event.queryStringParameters.page || 1;
+  const perPage = event.queryStringParameters.perPage || num;
+  const sort = event.queryStringParameters.sort || '-updateAt';
+  const filterQuery = event.queryStringParameters.filter || '{}';
+  const filter = JSON.parse(filterQuery);
+  const options = {
+    skip: (page * perPage) - perPage,
+    limit: parseInt(perPage, 10),
+    sort,
+  };
   try {
-    result = await Message.find();
+    result = await Message.find(filter, null, options);
   } catch (err) {
     return {
       statusCode: 400,
@@ -20,7 +34,9 @@ module.exports = async (event) => {
   return {
     statusCode: 200,
     headers: {
+      'Access-Control-Expose-Headers': 'X-Total-Count',
       'Content-Type': 'application/json',
+      'X-Total-Count': num,
     },
     body: JSON.stringify(result),
   };
