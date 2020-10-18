47f941dd9b0b381e4a7bd6706bede1a5d4e8bb16
Kerstin Huppenbauer <k.huppenbauer@web.de>
2020-10-18 11:09:24 +0200

    TASK: Optimize React Admin Filter

--- src/components/admin/resources/files/filesFilter.js
+++ src/components/admin/resources/files/filesFilter.js
@@ -6,19 +6,37 @@ import {
 } from 'react-admin';
 
 const FilesFilter = (props) => {
+  const facet = (field) => [
+    {
+      $group: {
+        _id: `$${field}`,
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        value: '$_id',
+        count: 1,
+      },
+    },
+    {
+      $sort: {
+        count: -1,
+      },
+    },
+  ];
+
   const pipeline = [
     {
       $match: props.filterValues,
     },
     {
-      $group: {
-        _id: 0,
-        mimeType: {
-          $addToSet: '$mimeType',
-        },
-        extension: {
-          $addToSet: '$extension',
-        },
+      $facet: {
+        mimeType: facet('mimeType'),
+        extension: facet('extension'),
       },
     },
   ];
@@ -32,21 +50,22 @@ const FilesFilter = (props) => {
       },
     },
   });
-  if (data === undefined) {
+  if (data === undefined || data.length === 0) {
     return [];
   }
+
   const { mimeType, extension } = data[0];
   return (
     <Filter {...props}>
       <SelectInput
         source="mimeType"
         key="mimeType-filter"
-        choices={mimeType.map((mimeTypeItem) => ({ id: mimeTypeItem, name: mimeTypeItem }))}
+        choices={mimeType.map((mimeTypeItem) => ({ id: mimeTypeItem.value, name: `${mimeTypeItem.value} (${mimeTypeItem.count})` }))}
       />
       <SelectInput
         source="extension"
         key="extension-filter"
-        choices={extension.map((extensionItem) => ({ id: extensionItem, name: extensionItem }))}
+        choices={extension.map((extensionItem) => ({ id: extensionItem.value, name: `${extensionItem.value} (${extensionItem.count})` }))}
       />
     </Filter>
   );
--- src/components/admin/resources/messages/messagesFilter.js
+++ src/components/admin/resources/messages/messagesFilter.js
@@ -6,22 +6,38 @@ import {
 } from 'react-admin';
 
 const MessagesFilter = (props) => {
+  const facet = (field) => [
+    {
+      $group: {
+        _id: `$${field}`,
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        value: '$_id',
+        count: 1,
+      },
+    },
+    {
+      $sort: {
+        count: -1,
+      },
+    },
+  ];
+
   const pipeline = [
     {
       $match: props.filterValues,
     },
     {
-      $group: {
-        _id: 0,
-        status: {
-          $addToSet: '$status',
-        },
-        app: {
-          $addToSet: '$app',
-        },
-        event: {
-          $addToSet: '$event',
-        },
+      $facet: {
+        app: facet('app'),
+        event: facet('event'),
+        status: facet('status'),
       },
     },
   ];
@@ -35,7 +51,7 @@ const MessagesFilter = (props) => {
       },
     },
   });
-  if (data === undefined) {
+  if (data === undefined || data.length === 0) {
     return [];
   }
   const { app, event, status } = data[0];
@@ -44,17 +60,17 @@ const MessagesFilter = (props) => {
       <SelectInput
         source="app"
         key="app-filter"
-        choices={app.map((appItem) => ({ id: appItem, name: appItem }))}
+        choices={app.map((appItem) => ({ id: appItem.value, name: `${appItem.value} (${appItem.count})` }))}
       />
       <SelectInput
         source="event"
         key="event-filter"
-        choices={event.map((eventItem) => ({ id: eventItem, name: eventItem }))}
+        choices={event.map((eventItem) => ({ id: eventItem.value, name: `${eventItem.value} (${eventItem.count})` }))}
       />
       <SelectInput
         source="status"
         key="status-filter"
-        choices={status.map((statusItem) => ({ id: statusItem, name: statusItem }))}
+        choices={status.map((statusItem) => ({ id: statusItem.value, name: `${statusItem.value} (${statusItem.count})` }))}
       />
     </Filter>
   );
