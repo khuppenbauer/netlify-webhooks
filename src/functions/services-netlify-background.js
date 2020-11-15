c1f3ef14db32e0393b86001c54a60794ac83df20
Kerstin Huppenbauer <k.huppenbauer@web.de>
2020-11-15 21:29:37 +0100

    TASK: Add netlify background function for filesync

---
+++ src/functions/services-netlify-background.js
@@ -0,0 +1,117 @@
+const axios = require('axios');
+const File = require('./models/file');
+const netlify = require('./services/netlify');
+const filesLib = require('./libs/files');
+
+const netlifyBaseUrl = 'https://api.netlify.com/api/v1/';
+const netlifyDeployEndpoint = `sites/${process.env.NETLIFY_CDN_ID}/deploys`
+const token = process.env.NETLIFY_ACCESS_TOKEN;
+
+const upload = async (event, message, id, required) => {
+  const query = {
+    sha1: {
+      $in: required,
+    },
+  };
+  const missingFiles = await File.find(query);
+  await missingFiles.reduce(async (lastPromise, item) => {
+    const accum = await lastPromise;
+    const {
+      name,
+      path_display,
+      foreignKey,
+      sha1,
+      extension,
+      externalUrl,
+    } = item;
+    const messageObject = {
+      ...event,
+      body: JSON.stringify({
+        id,
+        name,
+        path_display,
+        foreignKey,
+        sha1,
+        extension,
+        externalUrl,
+      }),
+    };
+    await netlify.upload(messageObject, message);
+    return [...accum, {}];
+  }, Promise.resolve([]));
+}
+
+const syncFiles = async (event, uploadMessage) => {
+  const files = await File.find({});
+  const newFiles = files.filter((file) => file.status === 'new');
+  if (newFiles.length === 0) {
+    return;
+  }
+  const filesData = files.reduce(
+    (acc, item) => (acc[item.path_display] = item.sha1, acc),
+    {},
+  );
+  const body = {
+    files: filesData,
+  };
+  const res = await axios({
+    method: 'post',
+    url: `${netlifyBaseUrl}${netlifyDeployEndpoint}`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    data: JSON.stringify(body),
+  });
+  const { id, required } = res.data;
+  if (required.length > 0) {
+    await upload(event, uploadMessage, id, required);
+  }
+  const query = {
+    sha1: {
+      $nin: required,
+    },
+    status: 'new',
+  };
+  const deployedFiles = await File.find(query);
+  await deployedFiles.reduce(async (lastPromise, file) => {
+    const {
+      name,
+      path_display,
+      foreignKey,
+      sha1,
+      extension,
+      externalUrl,
+    } = file;
+    const messageObject = {
+      ...event,
+      body: JSON.stringify({
+        name,
+        path_display,
+        foreignKey,
+        sha1,
+        extension,
+        externalUrl,
+      }),
+    };
+    const accum = await lastPromise;
+    await filesLib.message(messageObject, uploadMessage, { extension, path_display });
+    return [...accum, {}];
+  }, Promise.resolve([]));
+  await File.updateMany(query, { status: 'deployed' });
+};
+
+exports.handler = async (event) => {
+  if (event.httpMethod === 'POST') {
+    const uploadMessage = 'upload_{{dir}}_{{extension}}_file';
+    await syncFiles(event, uploadMessage);
+    return {
+      statusCode: 200,
+      body: 'Ok',
+    };
+  }
+  return {
+    statusCode: 405,
+    body: 'Method Not Allowed',
+  };
+};
--- src/functions/services/netlify/upload.js
+++ src/functions/services/netlify/upload.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const File = require('../../models/file');
 const filesLib = require('../../libs/files');
 const dropboxLib = require('../../libs/dropbox');
 
@@ -7,7 +8,7 @@ const token = process.env.NETLIFY_ACCESS_TOKEN;
 
 const uploadFiles = async (id, sha1, path_display, data) => {
   const netlifyFileUploadEndpoint = `deploys/${id}/files/${path_display}`;
-  await axios({
+  return axios({
     method: 'put',
     url: `${netlifyBaseUrl}${netlifyFileUploadEndpoint}`,
     headers: {
@@ -33,6 +34,9 @@ module.exports = async (event, message) => {
   } else {
     fileData = await dropboxLib.download(foreignKey);
   }
-  await uploadFiles(id, sha1, path_display, fileData);
-  await filesLib.message(event, message, data);
+  const res = await uploadFiles(id, sha1, path_display, fileData);
+  if (res.status === 200) {
+    await File.updateOne({ path_display }, { status: 'deployed' });
+    await filesLib.message(event, message, data);
+  }
 };
