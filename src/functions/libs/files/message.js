e7744da3807baa6cf0b5f91ab8a8ab7b83d0a32f
Kerstin Huppenbauer <k.huppenbauer@web.de>
2020-11-15 21:49:16 +0100

    BUGFIX: Better check for existing file message

--- src/functions/libs/files/message.js
+++ src/functions/libs/files/message.js
@@ -14,7 +14,7 @@ const createMessage = async (event, message, data) => {
   const { dir } = path.parse(path_display);
   const mapObj = { '{{dir}}': dir.replace('/', ''), '{{extension}}': extension };
   const eventMessage = await replaceAll(message, mapObj);
-  const existing = await Message.find({ foreignKey: path_display });
+  const existing = await Message.find({ foreignKey: path_display, app: 'netlify', event: eventMessage });
   if (existing.length === 0) {
     await messages.create(event, { foreignKey: path_display, app: 'netlify', event: eventMessage });
   }
