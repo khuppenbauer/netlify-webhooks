07ca8606574a4ff328a9be239f0f83ba9e68a3d3
Kerstin Huppenbauer <k.huppenbauer@web.de>
2021-02-22 07:26:52 +0100

    TASK: Connect Assets to Tracks in GraphCMS

--- src/functions/libs/graphcms/asset.js
+++ src/functions/libs/graphcms/asset.js
@@ -8,21 +8,17 @@ const File = require('../../models/file');
 const url = process.env.GRAPHCMS_API_URL;
 const token = process.env.GRAPHCMS_API_TOKEN;
 
-module.exports = async (data) => {
-  const { _id: file } = data;
-  const record = await File.findById(file);
-  const { externalUrl, coords, sha1 } = record;
-  let asset;
-
-  const graphcms = new GraphQLClient(
-    url,
-    {
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
+const graphcms = new GraphQLClient(
+  url,
+  {
+    headers: {
+      authorization: `Bearer ${token}`,
     },
-  );
+  },
+);
 
+const uploadAsset = async (record) => {
+  const { externalUrl, sha1 } = record;
   if (externalUrl) {
     const query = gql`
       query getAsset($sha1: String!) {
@@ -48,66 +44,205 @@ module.exports = async (data) => {
         },
         data: `url=${encodeURIComponent(externalUrl)}`,
       });
-      asset = res.data.id;
-    } else {
-      asset = assetObj.id;
+      return res.data.id;
     }
+    return assetObj.id;
   }
-  if (asset) {
-    const mutation = gql`
-      mutation AddAsset(
-        $id: ID!,  
-        $source: Json, 
-        $dateTimeOriginal: DateTime, 
-        $extension: String, 
-        $folder: String,
-        $externalUrl: String,
-        $foreignKey: String,
-        $sha1: String,
-        $location: LocationInput,
-      ) {
-        updateAsset(
-          where: { id: $id }
-          data: {
-            source: $source,
-            dateTimeOriginal: $dateTimeOriginal,
-            extension: $extension,
-            folder: $folder,
-            externalUrl: $externalUrl,
-            foreignKey: $foreignKey,
-            sha1: $sha1,
-            location: $location,
-          }
-        ) {
-          id  
-          source
-          dateTimeOriginal
-          extension
-          folder
-          externalUrl
-          foreignKey
-          sha1
-          location {
-            latitude
-            longitude
-          }  
-        }
-      }  
-    `;
+  return false;
+};
 
-    let mutationVariables = {
-      ...record._doc,
-      id: asset,
+const updateAsset = async (asset, record) => {
+  const { coords } = record;
+  const mutation = gql`
+    mutation UpdateAsset(
+      $id: ID!,  
+      $source: Json, 
+      $dateTimeOriginal: DateTime, 
+      $extension: String, 
+      $folder: String,
+      $externalUrl: String,
+      $foreignKey: String,
+      $sha1: String,
+      $location: LocationInput,
+    ) {
+      updateAsset(
+        where: { id: $id }
+        data: {
+          source: $source,
+          dateTimeOriginal: $dateTimeOriginal,
+          extension: $extension,
+          folder: $folder,
+          externalUrl: $externalUrl,
+          foreignKey: $foreignKey,
+          sha1: $sha1,
+          location: $location,
+        }
+      ) {
+        id  
+        source
+        dateTimeOriginal
+        extension
+        folder
+        externalUrl
+        foreignKey
+        sha1
+        location {
+          latitude
+          longitude
+        }  
+      }
+    }  
+  `;
+
+  let mutationVariables = {
+    ...record._doc,
+    id: asset,
+  };
+  if (coords) {
+    mutationVariables = {
+      ...mutationVariables,
+      location: {
+        latitude: coords.lat,
+        longitude: coords.lon,
+      },
     };
-    if (coords) {
-      mutationVariables = {
-        ...mutationVariables,
-        location: {
-          latitude: coords.lat,
-          longitude: coords.lon,
-        },
-      };
+  }
+  const res = await graphcms.request(mutation, mutationVariables);
+  return res;
+};
+
+const publishAsset = async (asset) => {
+  const mutation = gql`
+    mutation PublishAsset(
+      $id: ID!,  
+    ) {
+      publishAsset(
+        where: { id: $id }
+      ) {
+        id
+        stage
+      }
+    }  
+  `;
+
+  const mutationVariables = {
+    id: asset,
+  };
+  const res = await graphcms.request(mutation, mutationVariables);
+  return res;
+};
+
+const createMultipleReferenceMutation = async (property) => (
+  gql`
+    mutation ConnectAsset(
+      $id: ID!
+      $name: String!,  
+    ) {
+      upsertTrack(
+        where: { name: $name }
+        upsert: {
+          create: {
+            ${property}: { 
+              connect: {
+                id: $id
+              }
+            }
+            name: $name
+          }
+          update: {
+            ${property}: { 
+              connect: {
+                where: {
+                  id: $id
+                }
+                position: {
+                  end: true
+                }
+              }
+            }
+          }
+
+        }
+      ) {
+        id
+      }
+    }  
+  `
+);
+
+const createSingleReferenceMutation = async (property) => (
+  gql`
+    mutation ConnectAsset(
+      $id: ID!
+      $name: String!,  
+    ) {
+      upsertTrack(
+        where: { name: $name }
+        upsert: { 
+          create: {
+            ${property}: { 
+              connect: {
+                id: $id
+              }
+            }
+            name: $name
+          }
+          update: {
+            ${property}: { 
+              connect: {
+                id: $id
+              }
+            }
+          }
+        }
+      ) {
+        id
+      }
+    }  
+  `
+);
+
+const updateTrack = async (asset, record, mutation) => {
+  const { source } = record;
+  const { foreignKey } = source;
+
+  const mutationVariables = {
+    id: asset,
+    name: foreignKey,
+  };
+  const res = await graphcms.request(mutation, mutationVariables);
+  return res;
+};
+
+module.exports = async (data) => {
+  const { _id: file } = data;
+  const record = await File.findById(file);
+  const { folder, extension } = record;
+  const asset = await uploadAsset(record);
+  if (asset) {
+    const res = await updateAsset(asset, record);
+    let mutation;
+    if (folder === '/images') {
+      mutation = await createMultipleReferenceMutation('photos');
+    } else {
+      let property;
+      if (folder === '/preview') {
+        property = 'staticImage';
+      } else if (folder === '/tracks') {
+        if (extension === 'gpx') {
+          property = 'gpxFile';
+        } else if (extension === 'json') {
+          property = 'geoJsonFile';
+        }
+      } else if (folder === '/convert/gpx') {
+        property = 'gpxFileSmall';
+      }
+      mutation = await createSingleReferenceMutation(property);
     }
-    return graphcms.request(mutation, mutationVariables);
+    if (mutation) {
+      await updateTrack(asset, record, mutation);
+    }
+    await publishAsset(asset);
+    return res;
   }
 };
