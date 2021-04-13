038bc6adcef782b0bc50fb21511d27d798497a97
Kerstin Huppenbauer <k.huppenbauer@web.de>
2021-04-13 08:49:22 +0200

    TASK: Make Strava activity more flexible

--- src/functions/messagequeue-strava-background.js
+++ src/functions/messagequeue-strava-background.js
@@ -3,49 +3,52 @@ const strava = require('./services/strava');
 const messages = require('./methods/messages');
 const sentry = require('./libs/sentry');
 
-const processSegments = async (event, message, segmentEfforts) => {
+const processSegments = async (event, message, segmentEfforts, parseSegments, saveSegmentsGpx) => {
   await segmentEfforts.reduce(async (lastPromise, segmentEffort) => {
     const accum = await lastPromise;
     const { segment } = segmentEffort;
     const { id: foreignKey } = segment;
     const existing = await Feature.find({ foreignKey });
     if (existing.length === 0) {
-      const messageData = {
-        foreignKey,
-        app: 'strava',
-        event: message,
+      if (parseSegments) {
+        await strava.segment(event, segment, saveSegmentsGpx);
+      } else {
+        const messageData = {
+          foreignKey,
+          app: 'strava',
+          event: message,
+        };
+        const messageObject = {
+          ...event,
+          body: JSON.stringify(segment),
+        };
+        await messages.create(messageObject, messageData);
       }
-      const messageObject = {
-        ...event,
-        body: JSON.stringify(segment),
-      };
-      await messages.create(messageObject, messageData);
     }
     return [...accum, {}];
   }, Promise.resolve([]));
-}
+};
 
 const handler = async (event) => {
   if (event.httpMethod === 'POST') {
-    const { action } = event.queryStringParameters;
+    const {
+      action, includeSegments, parseSegments, saveSegmentsGpx, dropboxSync,
+    } = event.queryStringParameters;
     if (action === 'activity') {
-      const { includeSegments } = event.queryStringParameters;
       const message = 'save_activity';
       const activityData = await strava.activity(event, message);
       if (includeSegments === 'true') {
-        const { id, segment_efforts: segmentEfforts } = activityData;
-        const segmentsMessage = 'parse_segments';
-        await processSegments(event, segmentsMessage, segmentEfforts);
+        const { segment_efforts: segmentEfforts } = activityData;
+        await processSegments(event, 'parse_segments', segmentEfforts, parseSegments, saveSegmentsGpx);
+      }
+      if (dropboxSync === 'true') {
+        await strava.photos(event, 'save_photos', dropboxSync);
       }
     } else if (action === 'photos') {
-      const { dropboxSync } = event.queryStringParameters;
-      const message = 'save_photos';
-      await strava.photos(event, message, dropboxSync);
+      await strava.photos(event, 'save_photos', dropboxSync);
     } else if (action === 'create') {
-      const message = 'create_activity';
-      await strava.create(event, message);
+      await strava.create(event, 'create_activity');
     } else if (action === 'segment') {
-      const { saveSegmentsGpx } = event.queryStringParameters;
       const segment = JSON.parse(event.body);
       await strava.segment(event, segment, saveSegmentsGpx);
     }
