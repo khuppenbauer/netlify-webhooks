e306fb6c573c51d8adb5277c79eed913d9ec90d5
Kerstin Huppenbauer <k.huppenbauer@web.de>
2021-08-20 08:57:18 +0200

    TASK: Add hierarchical categories to algolia documents

--- src/functions/libs/algolia/add.js
+++ src/functions/libs/algolia/add.js
@@ -32,18 +32,68 @@ module.exports = async (data) => {
   }
   const client = algoliasearch(applicationID, adminAPIKey);
   const index = client.initIndex(indexName);
+  const object = {
+    objectID: feature,
+    name,
+    type,
+    source,
+    foreignKey,
+    city,
+    state,
+    country,
+    meta,
+    _geoloc: geoLoc,
+  };
+  let hierarchicalCategories = {};
+  const {
+    startCity, startState, startCountry, endCity, endState, endCountry,
+  } = meta;
+  if (startCity && startState && startCountry && endCity && endCountry && endState) {
+    hierarchicalCategories = {
+      'hierarchicalCategories.lvl0': [
+        startCountry,
+        endCountry,
+      ],
+      'hierarchicalCategories.lvl1': [
+        `${startCountry} > ${startState}`,
+        `${endCountry} > ${endState}`,
+      ],
+      'hierarchicalCategories.lvl2': [
+        `${startCountry} > ${startState} > ${startCity}`,
+        `${endCountry} > ${endState} > ${endCity}`,
+      ],
+    };
+  }
+  await index
+    .setSettings({
+      attributesForFaceting: [
+        'searchable(hierarchicalCategories)',
+        'searchable(hierarchicalCategories.lvl0)',
+        'searchable(hierarchicalCategories.lvl1)',
+        'searchable(hierarchicalCategories.lvl2)',
+        'searchable(type)',
+      ],
+      searchableAttributes: [
+        'city',
+        'country',
+        'state',
+        'meta.startCity',
+        'meta.startState',
+        'meta.startCountry',
+        'meta.endCity',
+        'meta.endState',
+        'meta.endCountry',
+        'name',
+        'type',
+      ],
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   await index
     .saveObject({
-      objectID: feature,
-      name,
-      type,
-      source,
-      foreignKey,
-      city,
-      state,
-      country,
-      meta,
-      _geoloc: geoLoc,
+      ...object,
+      ...hierarchicalCategories,
     })
     .then(({ objectID }) => {
       console.log(objectID);
