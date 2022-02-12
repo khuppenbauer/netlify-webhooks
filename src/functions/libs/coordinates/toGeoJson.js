659a72a811fd3d611a9ab53cec8a9cacdbb55fcb
Kerstin Huppenbauer <k.huppenbauer@web.de>
2022-02-12 17:27:36 +0100

    TASK: Keep geoJson coordTimes property

--- src/functions/libs/coordinates/toGeoJson.js
+++ src/functions/libs/coordinates/toGeoJson.js
@@ -8,6 +8,8 @@ module.exports = async (data, type) => {
     const geoJson = tj.gpx(xml);
     geoJson.features[0].properties.color = 'red';
     geoJson.features[0].properties.type = type;
+    const { coordinateProperties } = geoJson.features[0].properties;
+    geoJson.features[0].properties.coordTimes = coordinateProperties.times;
     return geoJson;
   }
   if (nodeName === 'kml') {
